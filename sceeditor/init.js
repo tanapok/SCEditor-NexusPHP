1636fea4a0b6fa6acc37b6a7fe87d950ab853bb8
Zefang Ding <me@tanapok.com>
2022-11-21 02:59:51 +0800

    modify locale to 'cn'

--- sceeditor/init.js
+++ sceeditor/init.js
@@ -5,4 +5,5 @@ sceditor.create(textarea, {
     style: 'minified/themes/content/default.min.css',
     toolbar: 'bold,italic,underline|left,center,right,justify|font,size,color,removeformat|image,link,unlink,bulletlist,code,quote|maximize,source',
     fonts: 'Arial,Arial Black,Comic Neue,Courier New,Georgia,Impact,Sans-serif,Serif,Times New Roman,Trebuchet MS,Verdana',
+    locale: 'cn',
 });
