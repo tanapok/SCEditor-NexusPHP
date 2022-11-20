02645268d7a19ff8bebf3eafd27a16fec693936a
Zefang Ding <me@tanapok.com>
2022-11-21 02:31:32 +0800

    build tests

---
+++ sceeditor/init.js
@@ -0,0 +1,6 @@
+var textarea = document.getElementById('descr');
+sceditor.create(textarea, {
+    format: 'bbcode',
+    icons: 'monocons',
+    style: 'minified/themes/content/default.min.css'
+});
