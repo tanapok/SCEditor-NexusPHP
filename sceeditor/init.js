0ae0bca1542a0d18d39681389c5bbc4ffdce4e19
Zefang Ding <me@tanapok.com>
2022-11-21 02:57:23 +0800

    modify toolbar

--- sceeditor/init.js
+++ sceeditor/init.js
@@ -2,5 +2,6 @@ var textarea = document.getElementById('descr');
 sceditor.create(textarea, {
     format: 'bbcode',
     icons: 'monocons',
-    style: 'minified/themes/content/default.min.css'
+    style: 'minified/themes/content/default.min.css',
+    toolbar: 'bold,italic,underline|left,center,right,justify|font,size,color,removeformat|image,link,unlink,bulletlist,code,quote|maximize,source',
 });
